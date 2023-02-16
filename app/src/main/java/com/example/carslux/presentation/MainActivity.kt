b369fa94ba072346735df2b67055af810cfa65b8
Kolyan99 <shuknikolay99@gmail.com>
2023-02-16 13:04:48 +0300

    Architecture, Repository, Interactor and RepositoryImpl

--- app/src/main/java/com/example/carslux/presentation/MainActivity.kt
+++ app/src/main/java/com/example/carslux/presentation/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.carslux.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.carslux.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
